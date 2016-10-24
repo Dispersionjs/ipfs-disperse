a03811cd57d146366052e691e0cccf47b8604219
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-10-24 12:12:45 -0700

    add IPFS add function and corresponing tests

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const child_process = require('child_process')
+const exec = child_process.exec
+const spawn = child_process.spawn
+const path = require('path');
+const disperse = {};
+
+disperse.ipfsAddPromise =  function(file) {
+  return new Promise((resolve, reject) => {
+    exec(`ipfs add '${file}'`, (error, stdout, stderr) => {
+      if (error) {
+        reject(new Error(`error in ipfsAddPromise: ${error}`));
+      } else {
+        let hashObj = disperse.makeHashObj(stdout);
+        resolve(hashObj);
+      }
+    });
+  })
+}
+
+disperse.makeHashObj = function(hashStr) {
+  var hashArray = hashStr.split(' ');
+  var hashObj = {
+    [hashArray[1]]: {
+      "file": hashArray.slice(2).join(' ').trim(),
+      "time": new Date().toUTCString(),
+      "url": "https://ipfs.io/ipfs/" + hashArray[1]
+    }
+  }
+  return hashObj;
+}
+
+module.exports = disperse;
---
+++ test/test.js
@@ -0,0 +1,52 @@
+var assert = require('assert');
+var disperse = require('../index.js');
+var chai = require('chai');
+//var chaiAsPromised = require("chai-as-promised");
+//chai.use(chaiAsPromised);
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should;
+
+describe('makeHashObj', function () {
+  describe('when passed output from IPFS add', function () {
+    let sampleString = "added QmdjC7zjKi7pYoo3YatWL6pNvJqDxAZJhXBBeXzXhzhEwp pedal.png";
+    let hash = 'QmdjC7zjKi7pYoo3YatWL6pNvJqDxAZJhXBBeXzXhzhEwp';
+    let result = disperse.makeHashObj(sampleString);
+    let expectedUrl = 'https://ipfs.io/ipfs/QmdjC7zjKi7pYoo3YatWL6pNvJqDxAZJhXBBeXzXhzhEwp'
+    it('should return valid file name', function () {
+      assert.equal(result[hash].file, 'pedal.png');
+    });
+    it('should return timestamp', function () {
+      assert.equal(result[hash].time.length, 29);
+    });
+    it('should return valid url', function () {
+      assert.equal(result[hash].url, expectedUrl);
+    });
+  });
+});
+
+describe('ipfsAddPromise', function () {
+  this.timeout(15000);
+  let petalHash = 'QmdjC7zjKi7pYoo3YatWL6pNvJqDxAZJhXBBeXzXhzhEwp';
+  it('should return a correct file name', function (done) {
+    disperse.ipfsAddPromise("/Users/JasonRay/Desktop/pedal.png").then(function (data) {
+      expect(data[petalHash].file).to.equal('pedal.png');
+      done();
+    }).catch(function (error) {
+      done(error);
+    });
+  });
+});
+
+// describe('makeTinyUrlPromise', function () {
+//   this.timeout(15000);
+//   let sampleUrl = 'https://ipfs.io/ipfs/QmaC8Mu1ycE2NhfHSarDoW7CXCrdcKnDiDLsiumk79XLrz';
+//   it('should return a tiny url', function (done) {
+//     library.makeTinyUrlPromise(sampleUrl).then(function (data) {
+//       expect(data.substring(0, 18)).to.equal('http://tinyurl.com');
+//       done();
+//     }).catch(function (error) {
+//       done(error);
+//     });
+//   });
+// });
